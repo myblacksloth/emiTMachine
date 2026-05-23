1320d3c4c394b717b2e79f26265d8784029fad32
myblacksloth <25264956+myblacksloth@users.noreply.github.com>
2026-05-23 17:06:29 +0200

    utils: docker commands

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-# docker compose up --build
-docker compose -f docker-compose-reverse.yml up --build
+docker compose up --build
+# docker compose -f docker-compose-reverse.yml up --build
+
+# docker compose up -d --build backend
+# docker compose up -d --build frontend
+# docker compose up -d --build --force-recreate backend
+
+# avoid docker compose down
+
+# docker compose ps
+# docker compose logs -f backend
+# docker compose logs -f postgres
+
 
